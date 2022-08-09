29178ae3dcd60d4b304f833a01405f254acbcba3
David Evans <66533391+WhyMoreThanHow@users.noreply.github.com>
2022-08-09 21:18:05 +0000

    Task1 as comment added

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-'''Task 1
+'''
+Task 1:
+Create a lottery number generator. This must generate 6 random numbers between 1
+and 49 inclusive, and a bonus ball. The numbers must not repeat in any draw. Feel free
+to “prettify” the text output.
+Extension:
+If you found the above quite simple, you can add the extension task. The lottery is an
+example of a permutation without repetition. Your extension task is to get an input from
+the user containing all 7 numbers. If the input is valid, show the user which of their
+numbers were correct, and return the probability of him having chosen that combination
+of numbers.
 '''
 print('Start' )
 
